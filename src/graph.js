b8ee616256064f285db62753cfcd9500d4e3edc0
kevnster <kevxngyn@gmail.com>
2023-07-02 21:20:49 -0400

    added map and basic framework

---
+++ src/graph.js
@@ -0,0 +1,40 @@
+import { Node } from './node';
+
+class Graph {
+    constructor(nodes, edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    addNode(node) {
+
+    }
+
+    removeNode(node) {
+
+    }
+
+    addEdge(nodeA, nodeB) {
+
+    }
+
+    removeEdge(nodeA, nodeB) {
+
+    }
+
+    getNeighbors(node) {
+
+    }
+
+    getNodes() {
+        return this.nodes;
+    }
+
+    dijkstra(start) {
+
+    }
+
+    a_star(start) {
+
+    }
+}
---
+++ src/node.js
@@ -0,0 +1,16 @@
+export class Node {
+    constructor(data) {
+        this.observed = data[0];
+        this.location_details = data[1];
+        this.county = data[2];
+        this.state = data[3];
+        this.season = data[4];
+        this.title = data[5];
+        this.latitude = data[6];
+        this.longitude = data[7];
+        this.date = data[8];
+    }
+
+
+
+}
