2f6f089141b9cbbf05435d799ff29c1288cfded1
Tina Chi <96496840+tinabyte@users.noreply.github.com>
2023-07-27 20:09:27 -0400

    centered and added title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import MyMap from './myMap'; // Path to MyMap.js
+import React from "react";
+import MyMap from "./myMap"; // Path to MyMap.js
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <MyMap />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Operation BigFoot </h1>
+      {/* need to wrap map in map container div*/}
+      <MyMap />
+      <div className="mapStyle"></div>
+    </div>
+  );
 }
 
 export default App;
