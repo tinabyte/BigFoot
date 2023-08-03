488eb03fe157e5a3afc761b059ebf88977c112d7
kevnster <kevxngyn@gmail.com>
2023-08-03 09:02:18 -0500

    reorganized directories

---
+++ server/classes/graph.js
@@ -0,0 +1,15 @@
+const Node = require('node.js');
+
+class Graph {
+    constructor() {
+        this.nodes = {};
+    }
+
+    addNode(node) {
+        this.nodes.push(node);
+    }
+
+    knn(k) {
+
+    }
+}
